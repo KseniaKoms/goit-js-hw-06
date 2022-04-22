b57536e08932862b4f4d456682416abe52974143
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-04-22 14:02:31 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,10 +5,10 @@ inputRef.addEventListener('blur', onInputValueCheck);
 console.log(inputRef.value.length)
 
 function onInputValueCheck() {
-    if (inputRef.value.length === 6) {
-        inputRef.classList.add('valid')
-    } else 
-      inputRef.classList.add('invalid');
+    inputRef.value.length === 6
+        ? inputRef.classList.add('valid') & inputRef.classList.remove('invalid')
+        : inputRef.classList.add('invalid') & inputRef.classList.remove('valid');
        
 }
+
 
