3ba98f5dd1e86ce1b3f12e69b5a7dd92a50a6c87
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-04-23 22:03:46 +0300

    Update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -15,6 +15,6 @@ function onFormSubmit(event) {
         return alert("Please fill in all the fields!")
     }
 
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  console.log(`Email: ${email.value}; Password: ${password.value}`);
   event.currentTarget.reset();
 }
